15e1e9f24db54b29895dfc96ee96ffcd3e45a56c
王智博 <wangzhibo@bilibili.com>
2024-07-01 16:56:33 +0800

    [#99] unique engine refine code

--- src/Storages/MergeTree/UniqueMergeTreeIndexCommon.h
+++ src/Storages/MergeTree/UniqueMergeTreeIndexCommon.h
@@ -105,25 +105,8 @@ struct StringHashMapUniqueKeyIndex : public IUniqueKeyIndex
     size_t size() const override;
     void forEach(std::function<void(const StringRef &, const VersionAndRow &)> func) override;
     std::optional<VersionAndRow> get(const String & key) const override;
-    std::optional<VersionAndRow> get(const String & /*key*/, const bool & /*rowid_is_uinit32*/) const override { return {}; }
     std::vector<size_t> calculateTargetBuckets(const size_t & mod_bucket_num) const override;
     void serializeBinary(WriteBuffer & ostr, UniqueKeyBucketIndexPtr bucket_index) const override;
-    void serializeBinary(
-        const String & /*index_path*/,
-        Block & /*block*/,
-        const UniqueDeleteBitmapPtr & /*delete_bitmap*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const bool & /*rowid_is_uinit32*/,
-        const bool & /*is_same_key*/) const override
-    {
-    }
-    void serializeBinary(
-        const String & /*index_path*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const String & /*temp_unique_key_index_dir*/,
-        std::unique_ptr<rocksdb::DB> & /*temp_unique_key_index*/) const override
-    {
-    }
     void deserializeBinary(
         const DiskPtr & disk,
         const String & index_path,
@@ -132,9 +115,6 @@ struct StringHashMapUniqueKeyIndex : public IUniqueKeyIndex
         BucketIndexRangePtr bucket_range,
         size_t max_running_loading_task,
         size_t timeout_in_sec) override;
-    void deserializeBinary(const String & /*file_path*/, UniqueKeyIndexBlockCachePtr /*block_cache*/) override { }
-    UniqueKeyIterator newIterator(const IndexFile::ReadOptions & /*options*/) const override { return nullptr; }
-    size_t residentMemoryUsage() const override { return 0; }
 
 private:
     mutable UniqueKeyIndexMapType unique_key_index;
@@ -154,25 +134,8 @@ struct StandardMapUniqueKeyIndex : public IUniqueKeyIndex
     size_t size() const override;
     void forEach(std::function<void(const StringRef &, const VersionAndRow &)> func) override;
     std::optional<VersionAndRow> get(const String & key) const override;
-    std::optional<VersionAndRow> get(const String & /*key*/, const bool & /*rowid_is_uinit32*/) const override { return {}; }
     std::vector<size_t> calculateTargetBuckets(const size_t & mod_bucket_num) const override;
     void serializeBinary(WriteBuffer & ostr, UniqueKeyBucketIndexPtr bucket_index) const override;
-    void serializeBinary(
-        const String & /*index_path*/,
-        Block & /*block*/,
-        const UniqueDeleteBitmapPtr & /*delete_bitmap*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const bool & /*rowid_is_uinit32*/,
-        const bool & /*is_same_key*/) const override
-    {
-    }
-    void serializeBinary(
-        const String & /*index_path*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const String & /*temp_unique_key_index_dir*/,
-        std::unique_ptr<rocksdb::DB> & /*temp_unique_key_index*/) const override
-    {
-    }
     void deserializeBinary(
         const DiskPtr & disk,
         const String & index_path,
@@ -181,9 +144,6 @@ struct StandardMapUniqueKeyIndex : public IUniqueKeyIndex
         BucketIndexRangePtr bucket_range,
         size_t max_running_loading_task,
         size_t timeout_in_sec) override;
-    void deserializeBinary(const String & /*file_path*/, UniqueKeyIndexBlockCachePtr /*block_cache*/) override { }
-    UniqueKeyIterator newIterator(const IndexFile::ReadOptions & /*options*/) const override { return nullptr; }
-    size_t residentMemoryUsage() const override { return 0; }
 
 private:
     mutable UniqueKeyIndexMapType unique_key_index;
@@ -203,25 +163,8 @@ struct StandardUnOrderedMapUniqueKeyIndex : public IUniqueKeyIndex
     size_t size() const override;
     void forEach(std::function<void(const StringRef &, const VersionAndRow &)> func) override;
     std::optional<VersionAndRow> get(const String & key) const override;
-    std::optional<VersionAndRow> get(const String & /*key*/, const bool & /*rowid_is_uinit32*/) const override { return {}; }
     std::vector<size_t> calculateTargetBuckets(const size_t & mod_bucket_num) const override;
     void serializeBinary(WriteBuffer & ostr, UniqueKeyBucketIndexPtr bucket_index) const override;
-    void serializeBinary(
-        const String & /*index_path*/,
-        Block & /*block*/,
-        const UniqueDeleteBitmapPtr & /*delete_bitmap*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const bool & /*rowid_is_uinit32*/,
-        const bool & /*is_same_key*/) const override
-    {
-    }
-    void serializeBinary(
-        const String & /*index_path*/,
-        IndexFile::IndexFileInfo & /*file_info*/,
-        const String & /*temp_unique_key_index_dir*/,
-        std::unique_ptr<rocksdb::DB> & /*temp_unique_key_index*/) const override
-    {
-    }
     void deserializeBinary(
         const DiskPtr & disk,
         const String & index_path,
@@ -230,9 +173,6 @@ struct StandardUnOrderedMapUniqueKeyIndex : public IUniqueKeyIndex
         BucketIndexRangePtr bucket_range,
         size_t max_running_loading_task,
         size_t timeout_in_sec) override;
-    void deserializeBinary(const String & /*file_path*/, UniqueKeyIndexBlockCachePtr /*block_cache*/) override { }
-    UniqueKeyIterator newIterator(const IndexFile::ReadOptions & /*options*/) const override { return nullptr; }
-    size_t residentMemoryUsage() const override { return 0; }
 
 private:
     mutable UniqueKeyIndexMapType unique_key_index;
@@ -245,15 +185,7 @@ struct LevelDBUniqueKeyIndex : public IUniqueKeyIndex
 
     LevelDBUniqueKeyIndex() = default;
 
-    void initBucket(const size_t & /*bucket_num_*/) override { }
-    void add(const String & /*key*/, const VersionAndRow & /*value*/) override { }
-    bool empty() const override { return false; }
-    size_t size() const override { return 0; }
-    void forEach(std::function<void(const StringRef &, const VersionAndRow &)> /*func*/) override { }
-    std::optional<VersionAndRow> get(const String & /*key*/) const override { return {}; }
     std::optional<VersionAndRow> get(const String & key, const bool & rowid_is_uinit32) const override;
-    std::vector<size_t> calculateTargetBuckets(const size_t & /*mod_bucket_num*/) const override { return {}; }
-    void serializeBinary(WriteBuffer & /*ostr*/, UniqueKeyBucketIndexPtr /*bucket_index*/) const override { }
     void serializeBinary(
         const String & index_path,
         Block & block,
@@ -264,18 +196,8 @@ struct LevelDBUniqueKeyIndex : public IUniqueKeyIndex
     void serializeBinary(
         const String & index_path,
         IndexFile::IndexFileInfo & file_info,
-        const String & temp_unique_key_index_dir,
-        std::unique_ptr<rocksdb::DB> & temp_unique_key_index) const override;
-    void deserializeBinary(
-        const DiskPtr & /*disk*/,
-        const String & /*index_path*/,
-        UniqueKeyBucketIndexPtr /*bucket_index*/,
-        LoadingBucketPoolPtr /*loading_bucket_pool*/,
-        BucketIndexRangePtr /*bucket_range*/,
-        size_t /*max_running_loading_task*/,
-        size_t /*timeout_in_sec*/) override
-    {
-    }
+        const String & tmp_rocksdb_index_dir,
+        std::unique_ptr<rocksdb::DB> & tmp_rocksdb_index_writer) const override;
     void deserializeBinary(const String & file_path, UniqueKeyIndexBlockCachePtr block_cache) override;
     UniqueKeyIterator newIterator(const IndexFile::ReadOptions & options) const override;
     size_t residentMemoryUsage() const override;
