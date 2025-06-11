61e8bb7e6d7cff6c8a2161cdb9f3d2a69530d5a5
王智博 <wangzhibo@bilibili.com>
2025-06-11 22:00:12 +0800

    [#92] Add some bsi functions

--- src/Functions/bsi/registerFunctionsBsi.cpp
+++ src/Functions/bsi/registerFunctionsBsi.cpp
@@ -2,6 +2,8 @@
 #include "Functions/bsi/FunctionBsiOperator.h"
 #include "Functions/bsi/FunctionBsiSum.h"
 #include "Functions/bsi/FunctionBsiTopK.h"
+#include "Functions/bsi/FunctionBsiProductSum.h"
+#include "Functions/bsi/FunctionBsiSquareSum.h"
 
 namespace DB
 {
@@ -17,6 +19,8 @@ void registerFunctionsBsi(FunctionFactory & factory)
     factory.registerFunction<FunctionBsiTopK>();
     factory.registerFunction<FunctionBsiSum>();
     factory.registerFunction<FunctionBsiFilter>();
+    factory.registerFunction<FunctionBsiProductSum>();
+    factory.registerFunction<FunctionBsiSquareSum>();
 }
 
 }
