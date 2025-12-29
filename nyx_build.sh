0630910603f07a4afe9fc1c689a16662c032fc6c
王智博 <wangzhibo@bilibili.com>
2025-12-29 17:23:09 +0800

    Update nyx build for dev-22

--- nyx_build.sh
+++ nyx_build.sh
@@ -2,12 +2,11 @@
 
 echo "start building"
 
+git submodule sync --recursive
 git submodule update --init --recursive
 mkdir -p build
 cd build
-export CC=clang-14
-export CXX=clang++-14
-cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_LIBHDFS=1
+/data/app/cmake/cmake-3.16.3/bin/cmake .. -DCMAKE_C_COMPILER=/usr/bin/clang-14 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-14 -DCMAKE_BUILD_TYPE=Release -DUSE_LIBHDFS=1
 ninja
 
 echo "end build"
